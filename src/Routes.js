f0677aae4c6c395c24d312dd8a594fd871425889
Peter L Gunter Jr <petlgunjr@gmail.com>
2019-10-17 14:00:05 -0700

    Add note page up, error code 403 at login

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
 
 export default function Routes({ appProps }) {
@@ -12,6 +13,7 @@ export default function Routes({ appProps }) {
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
       <Route component={NotFound} />
     </Switch>
   );
