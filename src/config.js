398b73b6b580c464841472a8ff37199699e8fa7d
Peter L Gunter Jr <petlgunjr@gmail.com>
2019-10-17 11:54:26 -0700

    Add S3 bucket query

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "petlgunjr"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://sbigg1dibe.execute-api.us-east-2.amazonaws.com/prod/notes"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_lGPZ9Jt6k",
+    APP_CLIENT_ID: "5bjh5mb6qm2oisggh74covo0jr",
+    IDENTITY_POOL_ID: "us-west-2:9e2d1459-743c-42e7-bb51-9ff25a9d0a1c"
+  }
+};
